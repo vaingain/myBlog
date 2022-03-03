d9747976419814de4f5d927a04864cff4394493c
Huasansan <1306494165@qq.com>
2022-03-03 15:09:47 +0800

    test

---
+++ src/components/testcomponents/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div><router-link to="/">Home</router-link></div>
+    <div><router-link to="/about">About</router-link></div>
+    <div><router-link to="/blog">Bolg</router-link></div>
+    <div><router-link to="/helloworld">HelloWord</router-link></div>
+</template>
+
+<script setup>
+    import {useRoute,useRouter} from 'vue-router'
+    let route = useRoute()
+    let router = useRouter()
+</script>
+
+<style scoped>
+
+</style>
